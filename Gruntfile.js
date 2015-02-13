544ad897a7593156fc73de92c21d6f604ed26df0
hugo <hugo@soixantecircuits.fr>
2015-02-13 18:06:23 +0100

    setup bootstrap and config

--- Gruntfile.js
+++ Gruntfile.js
@@ -222,7 +222,7 @@ module.exports = function (grunt) {
       server: {
         options: {
           port: 9001,
-          base: 'app/views',
+          base: ['app/', 'bower_components'],
           livereload: true
         }
       }
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 ```bash
 $ npm i
+$ bower install
 ```
 
 ## Usage
