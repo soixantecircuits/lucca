460e60705dca9cbfbf733844e73b9f6515d50172
gabrielstuff <gabriel@soixantecircuits.fr>
2015-04-24 02:23:12 +0200

    adding example

---
+++ app/config/config.example.js
@@ -0,0 +1,17 @@
+var config = {
+  raspberrypi: {
+    basename: "chow-chow",
+    port: 1337,
+    population: 64
+  },
+  streaming: true,
+  dev: true,
+   fakeFile:{
+    format:'',
+    zero:'00'
+  },
+  nuwa: {
+    address: 'http://127.0.0.1',
+    port: '3001'
+  }
+}
